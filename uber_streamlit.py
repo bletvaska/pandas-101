5a218849842c5304c6a370e44d146a5c938e2366
mirek <mirek@cnl.sk>
2023-12-15 13:43:52 +0100

    refactoring

--- uber_streamlit.py
+++ uber_streamlit.py
@@ -31,20 +31,29 @@ if st.checkbox('Zobraziť dáta') is True:
 
 '## Prehľad jázd za deň'
 
-date = st.date_input('**Vyber dátum:**', datetime.date(2015, 1, 1))
-day = 12
-month = 2
-year = 2014
+date = st.date_input('**Vyber dátum:**', datetime.date(2014, 1, 1))
 
 filter_date = (
-    (df['dt'].dt.year == year)
-    & (df['dt'].dt.month == month)
-    & (df['dt'].dt.day == day)
+    (df['dt'].dt.year == date.year)
+    & (df['dt'].dt.month == date.month)
+    & (df['dt'].dt.day == date.day)
 )
 
 df_filtered = df.loc[ filter_date, : ]
 
 data = df_filtered.groupby( df_filtered['dt'].dt.hour )['passengers'].count()
+profit = df_filtered.loc[ filter_date, 'fare' ].sum()
+
+f"Zisk: **${profit:.2f}**"
 st.bar_chart(data)
 
 
+f'## Nástupy na mape zo dňa {date}'
+
+hour_to_filter = st.slider('Vyber hodinu', 0, 23, 12)
+
+st.map(df_filtered, 
+       latitude='pickup_latitude', 
+       longitude='pickup_longitude',
+       # zoom=4
+)
